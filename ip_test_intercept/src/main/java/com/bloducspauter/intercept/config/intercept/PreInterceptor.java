0ef53b36d90b07fd36d7dd828ce3d8a3ea824ec3
Bloduc Spauter <3230695439@qq.com>
2024-05-16 22:40:38 +0800

    RedisConfig

--- ip_test_intercept/src/main/java/com/bloducspauter/intercept/config/intercept/PreInterceptor.java
+++ ip_test_intercept/src/main/java/com/bloducspauter/intercept/config/intercept/PreInterceptor.java
@@ -6,8 +6,11 @@ import com.bloducspauter.base.entities.FacilityInformation;
 import com.bloducspauter.base.page.PageParams;
 import com.bloducspauter.intercept.service.FacilityInformationService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -24,6 +27,9 @@ public class PreInterceptor implements HandlerInterceptor {
     @Resource
     FacilityInformationService service;
 
+    @Resource
+    RedisTemplate<String, Object> redisTemplate;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         return true;
@@ -37,6 +43,7 @@ public class PreInterceptor implements HandlerInterceptor {
             if (list.isEmpty()) {
                 log.warn("Empty forbidden entities");
             }
+            redisTemplate.opsForValue().set("forbidden_entities",list);
         } catch (Exception e) {
             log.debug("reason", e.getCause());
             log.error("loading  forbidden entities failed");

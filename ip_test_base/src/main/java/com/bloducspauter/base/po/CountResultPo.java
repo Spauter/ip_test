7d7afecfcefcbff6aa56fbed2af09da444d8bbed
Bloduc Spauter <3230695439@qq.com>
2024-05-17 12:58:00 +0800

    移除一个空模块

--- ip_test_base/src/main/java/com/bloducspauter/base/po/CountResultPo.java
+++ ip_test_base/src/main/java/com/bloducspauter/base/po/CountResultPo.java
@@ -1,8 +1,11 @@
-package com.bloducspauter.base.dto;
+package com.bloducspauter.base.po;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.util.Date;
 
 /**
  * 请求对象统计结果
@@ -11,7 +14,7 @@ import lombok.Data;
  */
 @Data
 @ApiModel("请求对象统计结果")
-public class CountResultDto {
+public class CountResultPo {
     @ApiModelProperty("总请求数量")
     private Integer totalRequests;
     @ApiModelProperty("总拒绝数量")
@@ -20,4 +23,7 @@ public class CountResultDto {
     private Integer ipAddress;
     @ApiModelProperty("总设备数量")
     private Integer totalId;
+    @ApiModelProperty("获取到此数据的时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
 }

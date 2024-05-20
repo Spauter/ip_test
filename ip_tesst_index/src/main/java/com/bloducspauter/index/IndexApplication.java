160fbfe762f5898e6e4236654753265d7531db19
Bloduc Spauter <3230695439@qq.com>
2024-05-20 15:04:51 +0800

    nacos

--- ip_tesst_index/src/main/java/com/bloducspauter/index/IndexApplication.java
+++ ip_tesst_index/src/main/java/com/bloducspauter/index/IndexApplication.java
@@ -1,4 +1,15 @@
 package com.bloducspauter.index;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Bloduc Spauter
+ *
+ */
+@SpringBootApplication
 public class IndexApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IndexApplication.class,args);
+    }
 }
--- ip_test_intercept/src/main/java/com/bloducspauter/intercept/App.java
+++ ip_test_intercept/src/main/java/com/bloducspauter/intercept/App.java
@@ -3,6 +3,7 @@ package com.bloducspauter.intercept;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 /**
  * 启动类
@@ -12,6 +13,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @SpringBootApplication(scanBasePackages={"com.bloducspauter.intercept"})
 @MapperScan(value = {"com.bloducspauter.intercept.mapper"})
 @EnableRedisHttpSession
+@EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
